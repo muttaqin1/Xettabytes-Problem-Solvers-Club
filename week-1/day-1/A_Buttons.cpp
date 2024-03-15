ae7fe673440f451412496f4ea2f7b0d5f3f3565b
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-03-15 22:25:11 +0600

    Solve A_Buttons problem.

---
+++ week-1/day-1/A_Buttons.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << max(max((a + a - 1), (b + b - 1)), (a + b)) << endl;
+    return 0;
+}
