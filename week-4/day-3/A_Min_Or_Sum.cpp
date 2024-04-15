9a913f0809aa9b948d782b39a71288a60f0e59db
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-04-15 13:38:35 +0600

    Solve A_Min_Or_Sum problem.

---
+++ week-4/day-3/A_Min_Or_Sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int res = 0;
+        fori(i, 0, n)
+        {
+            int a;
+            cin >> a;
+            res |= a;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
