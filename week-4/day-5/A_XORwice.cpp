d1db629bf5f1cad4c02148d472ffa790059b493f
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-04-17 09:27:09 +0600

    Solve A_XORwice problem.

---
+++ week-4/day-5/A_XORwice.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << (a ^ b) << endl;
+    }
+    return 0;
+}
