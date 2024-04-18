74528be31b06ecdb1fe349da319eb925bb883fa1
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-04-18 13:17:47 +0600

    Solve A_Mocha_and_Math problem.

---
+++ week-4/day-7/A_Mocha_and_Math.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        fori(i, 0, n) cin >> arr[i];
+        int res = arr[0];
+        for (int i = 1; i < n; i++)
+            res &= arr[i];
+        cout << res << endl;
+    }
+    return 0;
+}
