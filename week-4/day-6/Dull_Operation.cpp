7c7fb36a70efb9ba0b0c21c86d20ee2928c5496f
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-04-18 08:46:43 +0600

    Solve Dull_Operation problem.

---
+++ week-4/day-6/Dull_Operation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << n << " " << n - 1 << endl;
+    }
+
+    return 0;
+}
