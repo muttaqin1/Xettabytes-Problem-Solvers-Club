7e8e8d080484780c07fbc2da426a7fde0a8fe0c2
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-04-24 22:58:40 +0600

    Solve B_Bogosort problem.

---
+++ week-5/day-6/B_Bogosort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<ll> arr(n);
+        fori(i, 0, n) cin >> arr[i];
+        sort(arr.rbegin(), arr.rend());
+        for (auto v : arr)
+            cout << v << " ";
+        cout<<endl;
+    }
+    return 0;
+}
