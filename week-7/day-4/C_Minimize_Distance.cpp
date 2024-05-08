ad7e42afbcf1c21372cba865bd8c74f42821ff93
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-05-08 18:39:30 +0600

    Solve C_Minimize_Distance problem.

---
+++ week-7/day-4/C_Minimize_Distance.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+typedef long long int ll;
+#define Vlong std::vector<long long>
+#define Vint std::vector<int>
+#define Vchar std::vector<char>
+#define Vstring std::vector<string>
+#define Sint std::set<int>
+#define Slong std::set<long long int>
+#define pb push_back
+#define full(x) (x).begin(), (x).end()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+using namespace std;
+template <typename T, bool U = false>
+using Pq = std::priority_queue<T, std::vector<T>, std::conditional_t<U, greater<T>, less<T>>>;
+template <typename T>
+void look(vector<T> &arr);
+template <typename T>
+void look(stack<T> st);
+template <typename T>
+void look(Pq<T, true> pq);
+template <typename T>
+void look(Pq<T, false> pq);
+template <typename T>
+void look(queue<T> q);
+template <typename T>
+void look(list<T> &list);
+
+void Muttaqin()
+{
+    int n, k;
+    cin >> n >> k;
+    Vlong arr(n), pos, neg;
+    fori(i, 0, n) cin >> arr[i];
+    sort(full(arr));
+    fori(i, 0, n)
+    {
+        if (arr[i] > 0)
+        {
+            pos.push_back(arr[i]);
+        }
+        else if (arr[i] < 0)
+            neg.push_back(abs(arr[i]));
+    }
+    ll ans = 0;
+    for (int i = pos.size() - 1; i >= 0; i -= k)
+    {
+        ans += (pos[i] * 2);
+    }
+    for (int i = 0; i < neg.size(); i += k)
+    {
+        ans += (neg[i] * 2);
+    }
+    cout << ans - max(abs(arr[0]), abs(arr[n - 1])) << endl;
+}
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Muttaqin();
+    }
+    return 0;
+}
+template <typename T>
+void look(queue<T> q)
+{
+    cout << "{ ";
+    while (!q.empty())
+    {
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    cout << " }\n";
+}
+template <typename T>
+void look(Pq<T, false> pq)
+{
+    cout << "{ ";
+    while (!pq.empty())
+    {
+        cout << pq.top() << ", ";
+        pq.pop();
+    }
+    cout << " }\n";
+}
+template <typename T>
+void look(Pq<T, true> pq)
+{
+    cout << "{ ";
+    while (!pq.empty())
+    {
+        cout << pq.top() << ", ";
+        pq.pop();
+    }
+    cout << " }\n";
+}
+template <typename T>
+void look(stack<T> st)
+{
+    cout << "{ ";
+    while (!st.empty())
+    {
+        cout << st.top() << ", ";
+        st.pop();
+    }
+    cout << " }\n";
+}
+template <typename T>
+void look(vector<T> &arr)
+{
+    cout << "{ ";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+        if (i + 1 < arr.size())
+            cout << ",";
+    }
+    cout << " }\n";
+}
+template <typename T>
+void look(list<T> &list)
+{
+    cout << "{ ";
+    for (auto v : list)
+    {
+        cout << v;
+        cout << ",";
+    }
+    cout << " }\n";
+}
