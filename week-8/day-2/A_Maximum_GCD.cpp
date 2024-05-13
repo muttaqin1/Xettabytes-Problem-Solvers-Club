2617e70c3982e90a4976893510bde3ad17f4742c
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-05-13 22:43:53 +0600

    Solve A_Maximum_GCD problem.

---
+++ week-8/day-2/A_Maximum_GCD.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        while (n % 2 != 0)
+        {
+            n--;
+        }
+        cout << __gcd(n, n / 2) << endl;
+    }
+    return 0;
+}
