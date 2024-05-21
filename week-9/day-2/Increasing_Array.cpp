af1dfd5607441f22385365a7c47bc1b6f69f5a97
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-05-21 22:57:31 +0600

    Solve Increasing_Array problem.

---
+++ week-9/day-2/Increasing_Array.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+typedef long long int ll;
+#define Vlong std::vector<long long>
+#define Vint std::vector<int>
+#define Vchar std::vector<char>
+#define Vstring std::vector<string>
+#define Sint std::set<int>
+#define Slong std::set<long long int>
+#define full(x) (x).begin(), (x).end()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+using namespace std;
+template <typename T, bool U = false>
+using Pq = std::priority_queue<T, std::vector<T>, std::conditional_t<U, greater<T>, less<T>>>;
+
+void Muttaqin()
+{
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    fori(i, 0, n) cin >> arr[i];
+    ll cnt = 0;
+    fori(i, 1, n)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            cnt += abs(arr[i] - arr[i - 1]);
+            arr[i] = arr[i - 1];
+        }
+    }
+    cout << cnt << endl;
+}
+
+int main()
+{
+    fastread();
+    //    int t;
+    //    cin >> t;
+    //    while (t--)
+    //   {
+    Muttaqin();
+    //    }
+    return 0;
+}
