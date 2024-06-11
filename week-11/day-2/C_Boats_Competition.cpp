bbf2fa1ffdaba82d136569168f3f1047edc4c318
Muttaqin <MDMUTTAQIN20@GMAIL.COM>
2024-06-11 23:14:44 +0600

    Solve C_Boats_Competition problem.

---
+++ week-11/day-2/C_Boats_Competition.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fori(i, s, e) for (long long int i = s; i < e; i++)
+#define forei(i, s, e) for (long long int i = s; i <= e; i++)
+#define ford(i, s, e) for (long long int i = s; i > e; i--)
+#define fored(i, s, e) for (long long int i = s; i >= e; i--)
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+typedef long long int ll;
+#define Vlong std::vector<long long>
+#define Vint std::vector<int>
+#define Vchar std::vector<char>
+#define Vstring std::vector<string>
+#define Sint std::set<int>
+#define Slong std::set<long long int>
+#define full(x) (x).begin(), (x).end()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T, bool U = false>
+using Pq = std::priority_queue<T, std::vector<T>, std::conditional_t<U, greater<T>, less<T>>>;
+template <typename T, bool U = true>
+using pbds = tree<T, null_type, conditional_t<U, less<T>, less_equal<T>>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void Muttaqin()
+{
+    int n;
+    cin >> n;
+    Vlong freq(n + 1);
+    fori(i, 0, n)
+    {
+        ll a;
+        cin >> a;
+        freq[a]++;
+    }
+    ll res = 0;
+    forei(i, 2, 2 * n)
+    {
+        ll curr = 0;
+        fori(j, 1, (i + 1) / 2)
+        {
+            if ((i - j) > n)
+                continue;
+            curr += min(freq[i - j], freq[j]);
+        }
+        if ((i % 2) == 0)
+        {
+            curr += ((freq[i / 2]) / 2);
+        }
+        res = max(res, curr);
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Muttaqin();
+    }
+    return 0;
+}
